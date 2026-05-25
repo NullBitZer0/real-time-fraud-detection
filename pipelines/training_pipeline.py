dfb0face554b36650c27b9a61f752b543cd3a5ad
NullbitZer0 <adeeshapererabiz@gmail.com>
2026-05-25 13:36:35 +0530

    refactor : modify the trainining pipeline to work with dvc

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,66 +1,37 @@
+import hydra
 import pandas as pd
 
-from src.components.data_ingestion import DataIngestion
-from src.components.data_validation import DataValidation
-from src.components.preprocessing import DataPreprocessing
-from src.components.feature_engineering import FeatureEngineering
+from omegaconf import DictConfig
+
 from src.components.model_training import ModelTrainer
 
-from src.utils.logger import logging
 
+@hydra.main(
+    version_base=None,
+    config_path="../configs",
+    config_name="config"
+)
 
-if __name__ == "__main__":
+def main(config: DictConfig):
 
-    logging.info(
-        "Training pipeline started"
+    train_df = pd.read_csv(
+        "artifacts/train_processed.csv"
     )
 
-    # Ingestion
-    ingestion = DataIngestion()
-
-    artifact = ingestion.initiate_data_ingestion()
-
-    # Validation
-    validator = DataValidation()
-
-    validator.initiate_validation(
-        artifact.train_path
+    test_df = pd.read_csv(
+        "artifacts/test_processed.csv"
     )
 
-    # Preprocessing
-    preprocessor = DataPreprocessing()
-
-    train_df = preprocessor.initiate_preprocessing(
-        artifact.train_path
-    )
-
-    test_df = preprocessor.initiate_preprocessing(
-        artifact.test_path
-    )
-
-    # Feature Engineering
-    feature_engineer = FeatureEngineering()
-
-    train_df = (
-        feature_engineer
-        .initiate_feature_engineering(train_df)s
-    )
-
-    test_df = (
-        feature_engineer
-        .initiate_feature_engineering(test_df)
-    )
-
-    # Model Training
     trainer = ModelTrainer()
 
     metrics = trainer.initiate_model_training(
         train_df,
-        test_df
+        test_df,
+        config
     )
 
     print(metrics)
 
-    logging.info(
-        "Training pipeline completed"
-    )
+
+if __name__ == "__main__":
+    main()
