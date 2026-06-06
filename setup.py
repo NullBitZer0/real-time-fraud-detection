5b2582886b4ad6e0c5effd33015737793a382519
NullbitZer0 <adeeshapererabiz@gmail.com>
2026-06-07 00:14:25 +0530

    update setup py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="fraud_detection",
-    version="0.0.1",
+    version="0.1.1",
     author="NullBitZer0",
     packages=find_packages(),
     install_requires=requirements,
